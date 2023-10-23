01747d4e1bb86d1ed0d3e12e67cbdfac8a0de593
Uygun Bodur <uygun@dop.com.tr>
2023-10-23 15:44:51 +0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import { $query, ic, Opt, nat64, Result, Record, StableBTreeMap, $update, Vec, match } from 'azle';
+import { v4 as uuidv4 } from 'uuid';
+
+type Message = Record<{
+  id: string;
+  title: string;
+  body: string;
+  attachmentURL: string;
+  createdAt: nat64;
+  updatedAt: Opt<nat64>;
+}>;
+
+type MessagePayload = Record<{
+  title: string;
+  body: string;
+  attachmentURL: string;
+}>;
+
+const messageStorage = new StableBTreeMap<string, Message>(0, 44, 1024);
+
+/*
+$query;
+export function getMessages(): Result<Vec<Message>, string> {
+  return Result.Ok<Vec<Message>,string>(messageStorage.values());
+}
+
+
+$query;
+export function getMessage(id: string): Result<Message, string> {
+  return match(messageStorage.get(id), {
+    Some: (message) => Result.Ok<Message, string>(message),
+    None: () => Result.Err<Message, string>("Message not found")
+  });
+}
+
+$update;
+export function addMessage(payload: MessagePayload): Result<Message, string> {
+  const message: Message = { id: uuidv4(), createdAt: ic.time(), updatedAt: Opt.None, ...payload };
+  messageStorage.insert(message.id, message);
+  return Result.Ok(message);
+};
+
+$update;
+export function updateMessage(id: string, payload: MessagePayload): Result<Message, string> {
+  return match(messageStorage.get(id), {
+    Some: (message) => {
+      const updatedMessage = { ...message, ...payload, updatedAt: Opt.Some(ic.time()) };
+      messageStorage.insert(message.id, updatedMessage);
+      return Result.Ok<Message, string>(updatedMessage);
+    },
+    None: () => Result.Err<Message, string>("Message not found")
+  });
+}
+
+$update;
+export function deleteMessage(id: string): Result<Message, string> {
+  return match(messageStorage.remove(id), {
+    Some: (deletedMessage) => Result.Ok<Message, string>(deletedMessage),
+    None: () => Result.Err<Message, string>("Message not found")
+  });
+};
+
+*/
+
+
+globalThis.crypto = {
+  // @ts-ignore
+  getRandomValues: () => {
+    let array = new Uint8Array(32);
+    for (let i = 0; i < array.length; i++) {
+      array[i] = Math.floor(Math.random() * 256);
+    }
+    return array;
+  }
+};
+
